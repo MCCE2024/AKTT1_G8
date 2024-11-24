0f195494ebb1cf30a4e8d8a933f647a22685a986
chrisfhb <2410781006@hochschule-burgenland.at>
2024-11-24 18:46:31 +0100

    create cart-service

---
+++ cart-service/src/cart/cart.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+
+// Export the CartItem interface so it can be used in other files
+export interface CartItem {
+  productId: number;
+  quantity: number;
+}
+
+@Injectable()
+export class CartService {
+  private cart: CartItem[] = [];
+
+  addToCart(productId: number, quantity: number = 1): CartItem {
+    const existingItem = this.cart.find(item => item.productId === productId);
+
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      this.cart.push({ productId, quantity });
+    }
+
+    return this.cart.find(item => item.productId === productId)!;
+  }
+
+  getCart(): CartItem[] {
+    return this.cart;
+  }
+
+  removeFromCart(productId: number): CartItem[] {
+    this.cart = this.cart.filter(item => item.productId !== productId);
+    return this.cart;
+  }
+
+  clearCart(): CartItem[] {
+    this.cart = [];
+    return this.cart;
+  }
+}
